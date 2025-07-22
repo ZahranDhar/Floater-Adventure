c3d9fe8215b60d45ecf49796c4ac905549c53dee
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-07-22 23:48:02 +0530

    Vercel-Working

--- routes/Welcome.py
+++ routes/Welcome.py
@@ -9,14 +9,3 @@ class WelcomeRoutes:
     def renderHome():
       return redirect("/home")
     
-    @app.route('/test-db')
-    def test_db():
-        try:
-            mongo_url = os.getenv('MongoClient')
-            print("MongoClient env:", mongo_url)
-            client = MongoClient(mongo_url)
-            print("Databases:", client.list_database_names())
-            return "Success"
-        except Exception as e:
-            print("Error accessing MongoDB:", e)
-            return "MongoDB error occurred", 500
