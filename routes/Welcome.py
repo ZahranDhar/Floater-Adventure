b9de440ef4b19848a39bf8b3ef64dc336c854f60
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-07-22 23:29:13 +0530

    Debugging

--- routes/Welcome.py
+++ routes/Welcome.py
@@ -1,5 +1,6 @@
 from flask import render_template,redirect
-
+import os 
+from pymongo import MongoClient
 class WelcomeRoutes:
   def __init__(self,app,client):
 
@@ -8,11 +9,14 @@ class WelcomeRoutes:
     def renderHome():
       return redirect("/home")
     
-    @app.route('/testdb', methods=['GET'])
+    @app.route('/test-db')
     def test_db():
-      try:
-          print("Databases:", client.list_database_names())
-          return "Check Vercel logs for database list."
-      except Exception as e:
-          print("Error accessing MongoDB:", e)
-          return "MongoDB error occurred", 500
+        try:
+            mongo_url = os.getenv('MongoClient')
+            print("MongoClient env:", mongo_url)
+            client = MongoClient(mongo_url)
+            print("Databases:", client.list_database_names())
+            return "Success"
+        except Exception as e:
+            print("Error accessing MongoDB:", e)
+            return "MongoDB error occurred", 500
