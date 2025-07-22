0071a4540bd9a22ba9836a48320bbf744050fae9
Zahran Dhar <zahrannazirdhar@gmail.com>
2025-07-22 15:30:01 +0530

    inserted-js

---
+++ static/js/AddGallery.js
@@ -0,0 +1,19 @@
+let mediaIndex=0;
+
+function addMedia(){
+  mediaIndex++;
+  const container = document.getElementById("mediafiles")
+  const media =document.createElement(`media_file_${mediaIndex}`)
+  media.innerHTML=`
+  <input type="file" name="media_file_${mediaIndex}" accept="image/*" required class="w-full border p-2 rounded" />
+  `;
+  container.appendChild(media);
+}
+
+function removeMedia(){
+  const container=getElementById("mediafiles")
+  if (mediaIndex>0){
+    container.lastElementChild.remove();
+    mediaIndex--;
+  }
+}
